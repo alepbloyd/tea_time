7787fa54446aaa24d73a7a34b2df3ae221f3e2c2
Alex Boyd <alexanderleeboyd@gmail.com>
2022-10-31 20:08:04 -0400

    ab/Feat: tea model validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,8 @@
 class Tea < ApplicationRecord
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :temperature, presence: true
+  validates :brew_time, presence: true
+
 end
