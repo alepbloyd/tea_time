c9435afa4463d55d5a3b8ef2daa10f99d6f49d47
Alex Boyd <alexanderleeboyd@gmail.com>
2022-10-31 20:04:41 -0400

    ab/Feat: customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state_ab, presence: true
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :street_address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state_ab }
+  end
 end
