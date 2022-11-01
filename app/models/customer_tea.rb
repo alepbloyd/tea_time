b5b571e9ee84b4bba8620ac483c387f16d24129b
Alex Boyd <alexanderleeboyd@gmail.com>
2022-10-31 20:14:32 -0400

    ab/Feat: CustomerTea validations

--- app/models/customer_tea.rb
+++ app/models/customer_tea.rb
@@ -1,5 +1,11 @@
 class CustomerTea < ApplicationRecord
 
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :frequency, presence: true
+  validates :status, presence: true
   
+  validates :customer_id, presence: true
+  validates :tea_id, presence: true
 
 end
